f9503704e5b1473d73ed7cda7f1ed03c43281aa8
Nitin Dhyani <nitindhyani1234@gmail.com>
2024-07-29 21:39:38 +0530

    Calander

---
+++ script.js
@@ -0,0 +1,15 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+const weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const allmonths=["January","February","March","April","May","June","July","August","September","October","November","December"]
+console.log(today)
+
+date.innerHTML=(today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML=weekdays[today.getDay()];
+month.innerHTML=allmonths[today.getMonth()];
+year.innerHTML=today.getFullYear();
